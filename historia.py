894ec13477fd8e045df918609af88767c918fbe6
Alejandro <alejandrovalenciar3@gmail.com>
2025-08-04 18:58:38 -0600

    Parte 3 de la historia

--- historia.py
+++ historia.py
@@ -1,4 +1,4 @@
 #historia.py
 print("Parte 1: Juan salió de su casa muy temprano.")
 print("Parte 2: Se encontró con un dragón en la entrada del bosque.")# Parte 2
-# Parte 3
+print("Parte 3: Al final, el dragón resultó ser su mascota perdida.")# Parte 3
