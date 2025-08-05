a815bbac5c6568538f907acedb205ff3393368aa
Alejandro <alejandrovalenciar3@gmail.com>
2025-08-04 19:17:24 -0600

    Ups, borré todo sin querer

--- historia.py
+++
@@ -1,4 +0,0 @@
-#historia.py
-print("Parte 1: Juan salió de su casa muy temprano.")
-print("Parte 2: Se encontró con un dragón en la entrada del bosque.")# Parte 2
-print("Parte 3: Al final, el dragón resultó ser su mascota perdida.")# Parte 3
